f7c995e4f6cdb3a8789ee234311754cda379b809
panopticons <thepanopticons@gmail.com>
2024-01-10 09:51:27 -0700

    removed duplicate function and deleted uneeded page

--- src/components/MiniMenu.js
+++ src/components/MiniMenu.js
@@ -1,22 +1,14 @@
 'use client'
-import React from "react";
+import React, { useEffect } from "react";
 import styles from '../app/page.module.css'
 
 export default function MiniMenu() {
-    
-  async function sendMessage() {
-    login()
-    const message = document.getElementById("messageToSend").value
-    //document.getElementById("serverStatus").value = document.cookie
-    //localStorage.setItem("token", doc)
-    
-  }
 
     async function alive() {
       login()
       var result = localStorage.getItem("jwttoken")
       //alert("TO BE USED: " + result)
-      //alert(`Bearer ${result}`)
+      console.log(`Bearer ${result}`)
       const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
         method: 'GET',
         headers: {
@@ -30,42 +22,42 @@ export default function MiniMenu() {
     }
 
     // TBD
-    async function sendMessage() {
-      login()
-      var result = localStorage.getItem("jwttoken")
-      var message = document.getElementById("messageToSend").value
-      //alert("TO BE USED: " + result)
-      alert(`Bearer ${result}`)
-      const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${result}`
-        },
-
-        body: JSON.stringify({body: message})
-      });
-
-        const status = await response.text();
-        document.getElementById("messageResponse").value = status.status
-        return status
-    }
-
-    async function login() {
-      //alert("IN LOGIN")
-        var response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/login', {
-            method: 'GET'
-        });
-
-        var token = await response.text();
-        //alert("THE REAL !!!!!!!!!!! " + token)
-        localStorage.setItem("jwttoken", token)
-        //alert(localStorage.getItem("jwttoken") + " ___ current" )
-        return token
-    }
-
-    login().then(token => {
-      console.log(token)
+  async function sendMessage() {
+    login()
+    var result = localStorage.getItem("jwttoken")
+    var message = document.getElementById("messageToSend").value
+    //alert("TO BE USED: " + result)
+    console.log(`Bearer ${result}`)
+    const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${result}`
+      },
+      body: "body"
     });
+
+    const received = await response.text();
+    document.getElementById("messageResponse").value = received.status
+    return received
+  }
+
+  async function login() {
+    //alert("IN LOGIN")
+    var response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/login', {
+      method: 'GET'
+    });
+
+    var token = await response.text();
+    //alert("THE REAL !!!!!!!!!!! " + token)
+    localStorage.setItem("jwttoken", token)
+    //alert(localStorage.getItem("jwttoken") + " ___ current" )
+
+    return token
+  }
+
+  login().then(token => {
+    console.log(token)
+  });
 
   return (
     <main className={styles.MiniMenu}>
