9dd32b9c0c2691cd9c1d12055f2c759b73da535f
panopticons <thepanopticons@gmail.com>
2024-01-10 22:58:48 -0700

    changed sendMessage from form, sendMessage implemented

--- src/components/MiniMenu.js
+++ src/components/MiniMenu.js
@@ -1,42 +1,49 @@
 'use client'
-import React, { useEffect } from "react";
+import React from "react";
 import styles from '../app/page.module.css'
 
 export default function MiniMenu() {
+  async function alive() {
+    login().then(token => {
+      localStorage.setItem("jwttoken", token)
+    });
+    var result = localStorage.getItem("jwttoken")
+    var trimmedResult = result.substring(8, result.lastIndexOf("\""));
+    console.log(`Bearer ${trimmedResult}`)
+    const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${trimmedResult}`
+      },
+    });
 
-    async function alive() {
-      login()
-      var result = localStorage.getItem("jwttoken")
-      //alert("TO BE USED: " + result)
-      console.log(`Bearer ${result}`)
-      const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${result}`
-        },
-      });
-
-        const status = await response.text();
-        document.getElementById("serverStatus").value = status
-        return status
-    }
+    const status = await response.text();
+    document.getElementById("serverStatus").value = status
+    return status
+  }
 
     // TBD
   async function sendMessage() {
-    login()
+    login().then(token => {
+      localStorage.setItem("jwttoken", token)
+    });
     var result = localStorage.getItem("jwttoken")
-    var message = document.getElementById("messageToSend").value
+    var trimmedResult = result.substring(8, result.lastIndexOf("\""));
+    var message = {text: String(document.getElementById("messageToSend").value)}
     //alert("TO BE USED: " + result)
-    console.log(`Bearer ${result}`)
-    const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/alive', {
+   
+    console.log(`Bearer ${trimmedResult}`)
+    const response = await fetch('https://diva-challenge-ul4cm77qva-uc.a.run.app/slack', {
       method: 'POST',
       headers: {
-        Authorization: `Bearer ${result}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${trimmedResult}`
       },
-      body: "body"
+      body: JSON.stringify(message)
     });
 
-    const received = await response.text();
+    const received = await response;
+    //console.log("MESSAGE RESPONSE: " + received.status)
     document.getElementById("messageResponse").value = received.status
     return received
   }
@@ -49,29 +56,25 @@ export default function MiniMenu() {
 
     var token = await response.text();
     //alert("THE REAL !!!!!!!!!!! " + token)
-    localStorage.setItem("jwttoken", token)
+    //--localStorage.setItem("jwttoken", token)
     //alert(localStorage.getItem("jwttoken") + " ___ current" )
-
+    //console.log("FIRST TOKEN: " + token)
     return token
   }
-
-  login().then(token => {
-    console.log(token)
-  });
 
   return (
     <main className={styles.MiniMenu}>
       <h1 className={styles.h1}> We are in MiniMenu.</h1>
+
       <h2>Get server status</h2>
       <button className={styles.button} onClick={alive}>Check</button>
+      <h3>Status right now:</h3>
       <textarea id="serverStatus"></textarea>
 
-      <form onSubmit={sendMessage}>
-      <h2>Send a message</h2>
+=     <h2>Send a message</h2>
       <input id = "messageToSend" placeholder="Enter a message:"></input>
-      <button className={styles.button} type="submit">Send</button>
+      <button className={styles.button} onClick={sendMessage}>Send</button>
 
-      </form>
       <h2>Response:</h2>
       <textarea id = "messageResponse"></textarea>
     </main>
